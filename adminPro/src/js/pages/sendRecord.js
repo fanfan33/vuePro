3bbd76c4ef032345c0fe304e13b96b7704f84fad
fengf <fengf@hlongsky.com>
2018-01-12 12:12:30 +0800

    datep

--- adminPro/src/js/pages/sendRecord.js
+++ adminPro/src/js/pages/sendRecord.js
@@ -9,8 +9,20 @@ $('.sendRecord .btn-search').click(function() {
 	if ($(this).prev().val() == '') {
 		$('#myModal').modal('show')
 	}
+	
 })
-
+$('.btn-info').click(function() {
+	var startDate = $('#dtp_input1').val(),
+		endDate = $('#dtp_input2').val();
+	if (!startDate) {
+		$('#myModal').modal('show').find('.modal-body').text('请输入开始时间');
+	}else if(!endDate) {
+		$('#myModal').modal('show').find('.modal-body').text('请输入结束时间');
+	}else {
+		console.log(startDate)
+		console.log('提交成功')
+	}
+})
 $('.form_date_begin1').datetimepicker({
     language:  'zh-CN',
     weekStart: 1,
